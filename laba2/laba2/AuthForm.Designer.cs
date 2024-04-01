a1aa341e50b3d9001421b73194cdadb8a17f222f
y34hh <yourenot52@gmail.com>
2024-04-01 18:23:04 +0700

    users & menu v debug/bin zakinutb

---
+++ laba2/laba2/AuthForm.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace laba2
+{
+    partial class AuthForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.loginBox = new System.Windows.Forms.TextBox();
+            this.passBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.enterButton = new System.Windows.Forms.Button();
+            this.cancelButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // loginBox
+            // 
+            this.loginBox.Location = new System.Drawing.Point(322, 186);
+            this.loginBox.Name = "loginBox";
+            this.loginBox.Size = new System.Drawing.Size(157, 22);
+            this.loginBox.TabIndex = 0;
+            // 
+            // passBox
+            // 
+            this.passBox.Location = new System.Drawing.Point(322, 214);
+            this.passBox.Name = "passBox";
+            this.passBox.PasswordChar = '*';
+            this.passBox.Size = new System.Drawing.Size(157, 22);
+            this.passBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(270, 189);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 217);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Пароль";
+            // 
+            // enterButton
+            // 
+            this.enterButton.Location = new System.Drawing.Point(361, 242);
+            this.enterButton.Name = "enterButton";
+            this.enterButton.Size = new System.Drawing.Size(75, 23);
+            this.enterButton.TabIndex = 4;
+            this.enterButton.Text = "Вход";
+            this.enterButton.UseVisualStyleBackColor = true;
+            this.enterButton.Click += new System.EventHandler(this.enterButton_Click);
+            // 
+            // cancelButton
+            // 
+            this.cancelButton.Location = new System.Drawing.Point(713, 415);
+            this.cancelButton.Name = "cancelButton";
+            this.cancelButton.Size = new System.Drawing.Size(75, 23);
+            this.cancelButton.TabIndex = 5;
+            this.cancelButton.Text = "Отмена";
+            this.cancelButton.UseVisualStyleBackColor = true;
+            this.cancelButton.Click += new System.EventHandler(this.cancelButton_Click);
+            // 
+            // AuthForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cancelButton);
+            this.Controls.Add(this.enterButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.passBox);
+            this.Controls.Add(this.loginBox);
+            this.Name = "AuthForm";
+            this.Text = "AuthForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox loginBox;
+        private System.Windows.Forms.TextBox passBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button enterButton;
+        private System.Windows.Forms.Button cancelButton;
+    }
+}
---
+++ laba2/laba2/AuthForm.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace laba2
+{
+    public partial class AuthForm : Form
+    {
+        public AuthForm()
+        {
+            InitializeComponent();
+        }
+
+        // Обработчик нажатия кнопки "Отмена"
+        private void cancelButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        // Обработчик нажатия кнопки "Вход"
+        private void enterButton_Click(object sender, EventArgs e)
+        {
+            // Чтение всех строк из файла пользователей
+            string[] lines = File.ReadAllLines("users.txt");
+            // Создание словаря для хранения пар логин-пароль
+            Dictionary<string, string> users = new Dictionary<string, string>();
+
+            // Перебор каждой строки файла
+            foreach (string line in lines)
+            {
+                string[] parts = line.Split(' ');
+                // Проверка, является ли строка логином и паролем
+                if (line.StartsWith("#"))
+                {
+                    // Добавление пары логин-пароль в словарь
+                    users[parts[0]] = parts[1].Trim();
+                }
+            }
+
+            // Проверка наличия введенного логина в словаре
+            if (users.ContainsKey("#" + loginBox.Text))
+            {
+                // Проверка правильности введенного пароля
+                if (passBox.Text == users["#" + loginBox.Text])
+                {
+                    MessageBox.Show("Успешно!"); // Оповещение об успешном входе
+                    MainForm mainForm = new MainForm(loginBox.Text); // Создание главной формы с передачей логина
+                    mainForm.Show(); // Отображение главной формы
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный пароль!"); // Оповещение о неправильном пароле
+                }
+            }
+            else
+            {
+                MessageBox.Show("Неправильный логин!"); // Оповещение о неправильном логине
+            }
+        }
+    }
+}

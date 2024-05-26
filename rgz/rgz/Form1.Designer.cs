cd80f8629dc06a1d404fd0737fffd1558962c632
y34hh <yourenot52@gmail.com>
2024-05-26 20:12:57 +0700

    todo: lol

---
+++ rgz/rgz/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace rgz
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBoxSortType = new System.Windows.Forms.ComboBox();
+            this.buttonSort = new System.Windows.Forms.Button();
+            this.panelVisualize = new System.Windows.Forms.Panel();
+            this.buttonRegenArr = new System.Windows.Forms.Button();
+            this.textBoxArrElCnt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.labelIterCnt = new System.Windows.Forms.Label();
+            this.labelTimer = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBoxSortType
+            // 
+            this.comboBoxSortType.FormattingEnabled = true;
+            this.comboBoxSortType.Location = new System.Drawing.Point(131, 6);
+            this.comboBoxSortType.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBoxSortType.Name = "comboBoxSortType";
+            this.comboBoxSortType.Size = new System.Drawing.Size(160, 24);
+            this.comboBoxSortType.TabIndex = 0;
+            this.comboBoxSortType.SelectedIndexChanged += new System.EventHandler(this.ComboBoxSortType_SelectedIndexChanged);
+            this.comboBoxSortType.SelectedValueChanged += new System.EventHandler(this.comboBoxSortType_SelectedValueChanged);
+            // 
+            // buttonSort
+            // 
+            this.buttonSort.Location = new System.Drawing.Point(299, 3);
+            this.buttonSort.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonSort.Name = "buttonSort";
+            this.buttonSort.Size = new System.Drawing.Size(164, 28);
+            this.buttonSort.TabIndex = 1;
+            this.buttonSort.Text = "Сортировать массив";
+            this.buttonSort.UseVisualStyleBackColor = true;
+            this.buttonSort.Click += new System.EventHandler(this.ButtonSort_Click);
+            // 
+            // panelVisualize
+            // 
+            this.panelVisualize.Location = new System.Drawing.Point(16, 62);
+            this.panelVisualize.Margin = new System.Windows.Forms.Padding(4);
+            this.panelVisualize.Name = "panelVisualize";
+            this.panelVisualize.Size = new System.Drawing.Size(1013, 492);
+            this.panelVisualize.TabIndex = 2;
+            this.panelVisualize.Paint += new System.Windows.Forms.PaintEventHandler(this.panelVisualize_Paint);
+            // 
+            // buttonRegenArr
+            // 
+            this.buttonRegenArr.Location = new System.Drawing.Point(355, 33);
+            this.buttonRegenArr.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonRegenArr.Name = "buttonRegenArr";
+            this.buttonRegenArr.Size = new System.Drawing.Size(220, 28);
+            this.buttonRegenArr.TabIndex = 3;
+            this.buttonRegenArr.Text = "Перегенерировать массив";
+            this.buttonRegenArr.UseVisualStyleBackColor = true;
+            this.buttonRegenArr.Click += new System.EventHandler(this.buttonRegenArr_Click);
+            // 
+            // textBoxArrElCnt
+            // 
+            this.textBoxArrElCnt.Location = new System.Drawing.Point(248, 39);
+            this.textBoxArrElCnt.Name = "textBoxArrElCnt";
+            this.textBoxArrElCnt.Size = new System.Drawing.Size(100, 22);
+            this.textBoxArrElCnt.TabIndex = 4;
+            this.textBoxArrElCnt.Text = "50";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Тип сортировки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(229, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Количество элементов в массиве";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(932, 45);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Итерация: ";
+            // 
+            // labelIterCnt
+            // 
+            this.labelIterCnt.AutoSize = true;
+            this.labelIterCnt.Location = new System.Drawing.Point(1015, 45);
+            this.labelIterCnt.Name = "labelIterCnt";
+            this.labelIterCnt.Size = new System.Drawing.Size(14, 16);
+            this.labelIterCnt.TabIndex = 8;
+            this.labelIterCnt.Text = "0";
+            // 
+            // labelTimer
+            // 
+            this.labelTimer.AutoSize = true;
+            this.labelTimer.Location = new System.Drawing.Point(892, 45);
+            this.labelTimer.Name = "labelTimer";
+            this.labelTimer.Size = new System.Drawing.Size(24, 16);
+            this.labelTimer.TabIndex = 9;
+            this.labelTimer.Text = "0 с";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(835, 45);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(51, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Время:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1045, 567);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labelTimer);
+            this.Controls.Add(this.labelIterCnt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxArrElCnt);
+            this.Controls.Add(this.buttonRegenArr);
+            this.Controls.Add(this.panelVisualize);
+            this.Controls.Add(this.buttonSort);
+            this.Controls.Add(this.comboBoxSortType);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Sort Visualizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.ComboBox comboBoxSortType;
+        private System.Windows.Forms.Button buttonSort;
+        private System.Windows.Forms.Panel panelVisualize;
+        private System.Windows.Forms.Button buttonRegenArr;
+        private System.Windows.Forms.TextBox textBoxArrElCnt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label labelIterCnt;
+        private System.Windows.Forms.Label labelTimer;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ rgz/rgz/Form1.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace rgz
+{
+    public partial class Form1 : Form
+    {
+        private int[] dataArray;
+        private int arraySize;
+        private const int barWidth = 10;
+        private int iterCount = 0;
+        Stopwatch sw;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitUI();
+        }
+
+        private void InitUI()
+        {
+            comboBoxSortType.Items.AddRange(new string[]
+            {
+                "Вставкой",
+                "Пузырьком",
+                "Выбором",
+                "Слиянием"
+            });
+            comboBoxSortType.SelectedIndex = 0;
+
+            comboBoxSortType.SelectedIndexChanged += ComboBoxSortType_SelectedIndexChanged;
+
+            buttonSort.Click += ButtonSort_Click;
+
+            // Создаем массив данных, но не заполняем его
+            //dataArray = new int[Int32.Parse(textBoxArrElCnt.Text)];
+
+            // Подписываемся на событие загрузки формы
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Генерируем массив данных после загрузки формы
+            GenerateRandomArray();
+        }
+
+        private void ComboBoxSortType_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //GenerateRandomArray();
+        }
+
+        private void GenerateRandomArray()
+        {
+            arraySize = Int32.Parse(textBoxArrElCnt.Text);
+            dataArray = new int[arraySize];
+
+            if (panelVisualize == null)
+            {
+                return; // панель еще не инициализирована
+            }
+
+            Random rand = new Random();
+            for (int i = 0; i < arraySize; i++)
+            {
+                dataArray[i] = rand.Next(5, panelVisualize.Height);
+            }
+            panelVisualize.Invalidate();
+        }
+
+        private async void ButtonSort_Click(object sender, EventArgs e)
+        {
+            iterCount = 0;
+            labelIterCnt.Text = iterCount.ToString();
+            timer1.Equals(0);
+            timer1.Start();
+            buttonSort.Enabled = false;
+            buttonRegenArr.Enabled = false;
+            sw = new Stopwatch();
+            sw.Start();
+            switch (comboBoxSortType.SelectedItem.ToString())
+            {
+                case "Вставкой":
+                    await InsertionSort();
+                    break;
+                case "Пузырьком":
+                    await BubbleSort();
+                    break;
+                case "Выбором":
+                    await SelectionSort();
+                    break;
+                case "Слиянием":
+                    await MergeSort(0, dataArray.Length - 1);
+                    break;
+            }
+            buttonSort.Enabled = true;
+            buttonRegenArr.Enabled = true;
+            timer1.Stop();
+            sw.Stop();
+        }
+
+        private async Task InsertionSort()
+        {
+            for (int i = 1; i < dataArray.Length; i++)
+            {
+                iterCount++;
+                labelIterCnt.Text = iterCount.ToString();
+                int key = dataArray[i];
+                int j = i - 1;
+                while (j >= 0 && dataArray[j] > key)
+                {
+                    dataArray[j + 1] = dataArray[j];
+                    j--;
+                    panelVisualize.Invalidate();
+                    await Task.Delay(50);
+                }
+                dataArray[j + 1] = key;
+                panelVisualize.Invalidate();
+                await Task.Delay(50);
+            }
+        }
+
+        private async Task BubbleSort()
+        {
+            for (int i = 0; i < dataArray.Length - 1; i++)
+            {
+                iterCount++;
+                labelIterCnt.Text = iterCount.ToString();
+                for (int j = 0; j < dataArray.Length - i - 1; j++)
+                {
+                    if (dataArray[j] > dataArray[j + 1])
+                    {
+                        int temp = dataArray[j];
+                        dataArray[j] = dataArray[j + 1];
+                        dataArray[j + 1] = temp;
+                        panelVisualize.Invalidate();
+                        await Task.Delay(50);
+                    }
+                }
+            }
+        }
+
+        private async Task SelectionSort()
+        {
+            for (int i = 0; i < dataArray.Length - 1; i++)
+            {
+                iterCount++;
+                labelIterCnt.Text = iterCount.ToString();
+                int minIndex = i;
+                for (int j = i + 1; j < dataArray.Length; j++)
+                {
+                    if (dataArray[j] < dataArray[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int temp = dataArray[minIndex];
+                dataArray[minIndex] = dataArray[i];
+                dataArray[i] = temp;
+                panelVisualize.Invalidate();
+                await Task.Delay(50);
+            }
+        }
+
+        private async Task MergeSort(int left, int right)
+        {
+            iterCount++;
+            labelIterCnt.Text = iterCount.ToString();
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+                await MergeSort(left, middle);
+                await MergeSort(middle + 1, right);
+                await Merge(left, middle, right);
+            }
+        }
+
+        private async Task Merge(int left, int middle, int right)
+        {
+            int leftArraySize = middle - left + 1;
+            int rightArraySize = right - middle;
+
+            int[] leftArray = new int[leftArraySize];
+            int[] rightArray = new int[rightArraySize];
+
+            Array.Copy(dataArray, left, leftArray, 0, leftArraySize);
+            Array.Copy(dataArray, middle + 1, rightArray, 0, rightArraySize);
+
+            int i = 0, j = 0, k = left;
+
+            while (i < leftArraySize && j < rightArraySize)
+            {
+                if (leftArray[i] <= rightArray[j])
+                {
+                    dataArray[k++] = leftArray[i++];
+                }
+                else
+                {
+                    dataArray[k++] = rightArray[j++];
+                }
+                panelVisualize.Invalidate();
+                await Task.Delay(50);
+            }
+
+            while (i < leftArraySize)
+            {
+                dataArray[k++] = leftArray[i++];
+                panelVisualize.Invalidate();
+                await Task.Delay(50);
+            }
+
+            while (j < rightArraySize)
+            {
+                dataArray[k++] = rightArray[j++];
+                panelVisualize.Invalidate();
+                await Task.Delay(50);
+            }
+        }
+
+        private void panelVisualize_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            for (int i = 0; i < dataArray.Length; i++)
+            {
+                g.FillRectangle(Brushes.Blue, i * barWidth, panelVisualize.Height - dataArray[i], barWidth - 1, dataArray[i]);
+            }
+        }
+
+        private void buttonRegenArr_Click(object sender, EventArgs e)
+        {
+            GenerateRandomArray();
+        }
+
+        private void comboBoxSortType_SelectedValueChanged(object sender, EventArgs e)
+        {
+            GenerateRandomArray();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            labelTimer.Text = sw.Elapsed.Seconds.ToString() + " с";
+        }
+    }
+}
